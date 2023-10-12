e9932051efd96db323c2488801281d4cf9e36ddb
Yuliia <julia.tvenko@gmail.com>
2023-10-12 23:13:25 +0300

    lab2 updated functions

--- lab1/_RELATION.py
+++ lab1/_RELATION.py
@@ -1,12 +1,16 @@
 from abc import ABC, abstractmethod
 
-class RELATION(ABC):
+#class RELATION(ABC):
+class RELATION:
     def __init__(self, relations=None, size=None, type=None): 
         self.size = size
         if relations is not None:
-            self.relations = sorted(relations)
+            self.relations = set(sorted(relations))
             if size is None:
-                self.size = max(max(pair) for pair in self.relations) + 1
+                try:
+                    self.size = max(max(pair) for pair in self.relations) + 1
+                except ValueError:  
+                    self.size = 0
         else:
             if type is None or type == 'empty' or size is None:
                 self.relations = set()
@@ -18,6 +22,7 @@ class RELATION(ABC):
                 self.relations = {(i, j) for i in range(size) for j in range(size) if i != j}
             else:
                 self.relations = set()
+
 
     def __str__(self):
         return "{" + "\n".join(f"({x}, {y})" for x, y in sorted(self.relations)) + "}"
@@ -56,23 +61,111 @@ class RELATION(ABC):
     
     @abstractmethod
     def is_subset(self, other):
-        return RELATION(self.relations.issubset(other.relations))
+        return self.relations.issubset(other.relations)
+    
+    @abstractmethod
+    def is_reflexive(self):
+        return RELATION(size=self.size, type='diagonal').is_subset(self)
+    
+    @abstractmethod
+    def is_antireflexive(self):
+        return self.is_subset(RELATION(size=self.size, type='antidiagonal'))
+    
+    @abstractmethod
+    def is_symmetric(self):
+        return self.is_subset(self.converce())
+
+    @abstractmethod
+    def is_asymmetric(self):
+        return self.intersection(self.converce()).relations == set()
+    
+    @abstractmethod
+    def is_transitive(self):
+        return self.composition(self).is_subset(self) 
+    
+    @abstractmethod
+    def is_acyclic(self):
+        pass
+
+    @abstractmethod
+    def is_linear(self):
+        pass
 
 
-P = RELATION(relations={(0, 0), (0, 2), (1, 1), (1, 2), (1, 3), (2, 0), (2, 2), (2, 3), (3, 2), (3, 3)})
-Q = RELATION(relations={(0, 2), (0, 3), (1, 0), (1, 1), (1, 2), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2)})
+    @abstractmethod
+    def is_connected(self):
+        return self.union(self.converce()).difference(RELATION(size=self.size, type='diagonal')).relations \
+              == RELATION(size=self.size, type='antidiagonal').relations
+    
+    @abstractmethod
+    def is_tolerant(self):
+        return (self.is_reflexive()) and (self.is_symmetric())
+    
+    @abstractmethod
+    def is_equivalent(self):
+        return (self.is_reflexive()) and (self.is_symmetric()) and (self.is_transitive())
+    
+    @abstractmethod
+    def is_quasiorder(self):
+        return (self.is_reflexive()) and (self.is_transitive())
+    
+    @abstractmethod
+    def is_order(self):
+        return (self.is_reflexive()) and (self.is_asymmetric()) and (self.is_transitive())
+    
+    @abstractmethod
+    def is_strictorder(self):
+        return (self.is_asymmetric()) and (self.is_transitive())
+    
+    @abstractmethod
+    def is_linearorder(self):
+        return (self.is_reflexive()) and (self.is_asymmetric()) \
+            and (self.is_transitive()) and (self.is_connected()) 
+    
+    def symmetric_part(self):
+        return RELATION(self.relations.intersection(self.converce().relations))
 
+    def asymmetric_part(self):
+        return RELATION(self.relations.difference(self.symmetric_part().relations))
+    
+#Q = RELATION(relations={(0, 2), (0, 3), (1, 0), (1, 1), (1, 2), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2)})
+# Reflexivity
+# P = RELATION(relations={(0, 0), (0, 2), (1, 1), (1, 2), (1, 3), (2, 0), (2, 2), (2, 3), (3, 2), (3, 3)})
+# Antireflexivity
+# P = RELATION(relations={(0, 2), (1, 2), (1, 3), (2, 0), (2, 3), (3, 2)})
+# Symmetric
+# P = RELATION(relations={(0, 0), (0, 2), (2, 0), (2, 2), (2, 3), (3, 2), (3, 3)})
+# Asymmetric
+# P = RELATION(relations={(1, 2), (1, 3), (2, 0), (3, 2)})
+# Transitive
+# P = RELATION(relations={(1,2), (2,3), (1,3)})
+# Connected
+#P = RELATION(relations={(0,1),(0,2),(0,3),(1,0),(1,2),(1,3),(2,0),(2,1),(2,3),(3,0),(3,1),(3,2)})
+# Tolerant 
+#P = RELATION(relations={(0, 0), (0, 2), (1, 1), (2, 0), (2, 2), (2, 3), (3, 2), (3, 3)})
+# Equivalent
+P = RELATION(relations={(0, 0), (0, 2), (0, 3), (1, 1), (2, 0), (2, 2), (2, 3), (3, 0), (3, 2), (3, 3)})
 
-print('Intersection\n', P.intersection(Q))
-print('\nUnion\n', P.union(Q))
-print('\nDifference\n', P.difference(Q))
-print('\nSymmetric difference\n', P.sym_diff(Q))
-print('\nComposition\n', P.composition(Q))
-print('\nComplement\n', str(P.complement()))
-print('\nConverce\n', str(P.converce()))
-print('\nDual\n', str(P.dual()))
+# print('Intersection\n', P.intersection(Q))
+# print('\nUnion\n', P.union(Q))
+# print('\nDifference\n', P.difference(Q))
+# print('\nSymmetric difference\n', P.sym_diff(Q))
+# print('\nComposition\n', P.composition(Q))
+# print('\nComplement\n', str(P.complement()))
+# print('\nConverce\n', str(P.converce()))
+# print('\nDual\n', str(P.dual()))
 
+print('\nIs P reflexive\n', P.is_reflexive())
+print('\nIs P antireflexive\n', P.is_antireflexive())
+print('\nIs P symmetric\n', P.is_symmetric())
+print('\nIs P asymmetric\n', P.is_asymmetric())
+print('\nIs P transitive\n', P.is_transitive())
+print('\nIs P connected\n', P.is_connected())
+print('\nIs P tolerant\n', P.is_tolerant())
+print('\nIs P equivalent\n', P.is_equivalent())
 
+print('\nP symmetric part\n', P.symmetric_part())
+print('\nP asymmetric part\n', P.asymmetric_part())
 # print(str(RELATION(size=3, type='full')))
 # print(str(RELATION(size=3, type='diagonal')))
 # print(str(RELATION(size=3, type='antidiagonal')))
